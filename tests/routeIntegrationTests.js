8b244d8396573348a3b4c383459b93d6b5f3f925
Simon Situ <ssitu001@gmail.com>
2016-10-11 22:01:33 -0700

    add post test and change arrow functions

--- tests/routeIntegrationTests.js
+++ tests/routeIntegrationTests.js
@@ -5,23 +5,22 @@ const request = require('supertest');
 
 const expect = chai.expect;
 
-const PORT = process.env.PORT || 8080;
-const HOST = `http://localhost:${PORT}`;
+const HOST = 'http://localhost:8080';
 
 
 // test if this is necessary
 require('../server.js');
-
-describe('Server routes', () => {
-  describe('GET request to /', () => {
-    it('should respond with a status of 200', (done) => {
+console.log('process', process)
+describe('Server routes', function() {
+  describe('GET request to /', function() {
+    it('should respond with a status of 200', function(done) {
       request(HOST)
         .get('/')
         .expect('Content-Type', /text\/html/)
         .expect(200, done);
     });
 
-    it('should respond with the index.html file', (done) => {
+    it('should respond with the index.html file', function(done) {
       request(HOST)
         .get('/')
         .expect((response) => {
@@ -30,25 +29,44 @@ describe('Server routes', () => {
         })
         .expect(200, done);
     });
+  });
 
-    describe('GET request to /bundle.js', () => {
-      it('should respond with the bundle.js file', (done) => {
-        request(HOST)
-          .get('/bundle.js')
-          .expect((response) => {
-            const bundleFile = fs.readFileSync(path.join(__dirname, '../', 'client', 'bundle.js'));
-            expect(bundleFile.toString()).to.equal(response.text);
-          })
-          .expect(200, done);
-      });
-    });
-
-    describe('GET request to an invalid route', () => {
-      it('should respond with a status of 404', (done) => {
-        request(HOST)
-          .get('/bad')
-          .expect(404, done);
-      });
+  describe('GET request to /bundle.js', function() {
+    it('should respond with the bundle.js file', function(done) {
+      request(HOST)
+        .get('/bundle.js')
+        .expect((response) => {
+          const bundleFile = fs.readFileSync(path.join(__dirname, '../', 'client', 'bundle.js'));
+          expect(bundleFile.toString()).to.equal(response.text);
+        })
+        .expect(200, done);
     });
   });
+
+  describe('GET request to an invalid route', function() {
+    it('should respond with a status of 404', function(done) {
+      request(HOST)
+      .get('/bad')
+      .expect(404, done);
+    });
+  });
+  
+
+  describe('POST request to /save', function() {
+    it('should respond with a status of 200', function(done) {
+      request(HOST)
+        .post('/save')
+        .expect(200, done);
+    });
+  });
+
+  describe('POST request to /invalid', function() {
+    it('should respond with a status of 404', (done) => {
+      request(HOST)
+        .post('/invalid')
+        .expect(404, done);
+    });
+  });
+
 });
+
