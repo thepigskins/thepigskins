e08889bd4e976847c78903f75f2142be83f575b3
Dlaosb <Dlaosb@gmail.com>
2016-11-09 23:22:25 -0800

    Add Initial Sequelize Setup

--- server.js
+++ server.js
@@ -3,7 +3,30 @@ const path = require('path');
 const scraperController = require('./scraper'); 
 const app = express();
 const PORT = 8000;
+const database = require('./server/models/postgresDB');
 
+
+database.Userza.create({
+  userName: 'User43',
+  firstName: 'Bryan',
+  lastName: 'Daniel',
+  password: 'password'
+}).catch((error) => {
+  console.log(error);
+})
+
+// database.User.create({
+//   _id: 00000002,
+//   userName: 'User2',
+//   firstName: 'firstname2',
+//   lastName: 'lastname2',
+//   password: 'password'
+// }).catch((error) => {
+//   console.log(error); 
+// })
+
+//console.log('database', database);
+//
 app.use(express.static(path.join(__dirname, 'client', 'dist')));
 
 app.use((request, response, next) => {
