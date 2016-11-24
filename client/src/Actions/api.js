d422fb0cc90a4212146e7ea2ef7f39720416a2d8
Shawn Southwell <shawn@Shawns-MacBook-Pro.local>
2016-11-24 10:29:11 -0800

    Clean API file

--- client/src/Actions/api.js
+++ client/src/Actions/api.js
@@ -8,17 +8,18 @@ export function getPlayerData(playerName){
   [firstName, lastName] = playerName.trim().split(' ').map( (name) => name[0].toUpperCase().concat(name.slice(1)));
   const dbQuery = axios.get(`http://localhost:8000/findPlayer?firstName=${firstName}&lastName=${lastName}`);
   return (dispatch) => {
-		dbQuery.then((response) => {
+		dbQuery.then(response => {
 			dispatch({
 				type: REQUEST_PLAYER,
 				payload: response.data
         name: `${firstName} ${lastName}`
 			});
 		})
-		.catch((err) => {
+		.catch(err => {
 			dispatch({
-				type: FETCH_PLAYERS_ERR,
-				payload: err
+				type: REQUEST_PLAYER_ERR,
+				payload: err,
+        lol: true
 			})
 		});
 	};
