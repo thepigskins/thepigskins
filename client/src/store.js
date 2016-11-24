82a51539d5a662022d9e2948b84f73625a4fe1d5
Shawn Southwell <shawn@Shawns-MacBook-Pro.local>
2016-11-24 11:04:10 -0800

    Add thunk to handle async redux behavior

--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import mainReducer from './Reducers/reducer';
 import ReduxPromise from 'redux-promise';
+import thunk from 'redux-thunk';
 
 
 //http://redux.js.org/docs/advanced/AsyncFlow.html
-const store = applyMiddleware(ReduxPromise)(createStore)(mainReducer); 
+//const store = applyMiddleware(ReduxPromise)(createStore)(mainReducer);
 // const store = createStore(mainReducer);
 // createStore(mainReducer);
 // console.log("store", store)
 
-export default store;
+const middleware = applyMiddleware(promise, thunk);
+export default createStore(mainReducer, middleware);
