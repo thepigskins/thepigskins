00a93f73afabb4967aaa91958bab90fd7fec7183
Shawn Southwell <shawn@Shawns-MacBook-Pro.local>
2016-11-24 11:16:06 -0800

    Promote user input to container, move action call to user input

--- client/src/components/UserInput.jsx
+++ client/src/components/UserInput.jsx
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import Dropdown from './Dropdown';
+import { getPlayerData } from ''
+import { connect } from 'react-redux';
 
+ @connect((store) => {});
 
-export default (props) => {
-  console.log('props', props)
-  return (
-    <div>
-      <SearchBar getNewPlayer={props.getNewPlayer}/>
-      <Dropdown />
-    </div>
-  )
-}
+ export default class UserInput extends Component{
+   constructor(props){
+     super(props);
+     this.state = { playerName : '' }
+     this.handleChange = this.handleChange.bind(this);
+     this.formSubmit = this.formSubmit.bind(this);
+   }
+
+   handleChange(e){
+     this.setState({playerName : e.target.value})
+   }
+
+   formSubmit(e){
+     e.preventDefault();
+     console.log('form submit firing with this query', this.state.playerName)
+     this.props.dispatch(getPlayerData(this.state.playerName));
+   }
+
+   componentWillUpdate(){
+     this.setState({ playerName : '' });
+   }
+
+   render(){
+     return (
+       <div>
+        <SearchBar getNewPlayer={this.formSubmit}/>
+        <Dropdown />
+       </div>
+        )
+   }
+ }
